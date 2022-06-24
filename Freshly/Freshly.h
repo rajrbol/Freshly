e982b934d05be96587ab7e12a310fcde165b1d9e
rajrbol <raj.raval@lji.io>
2022-06-24 14:32:33 +0530

    Intial project setup

---
+++ Freshly/Freshly.h
@@ -0,0 +1,18 @@
+//
+//  Freshly.h
+//  Freshly
+//
+//  Created by Raj Raval on 24/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Freshly.
+FOUNDATION_EXPORT double FreshlyVersionNumber;
+
+//! Project version string for Freshly.
+FOUNDATION_EXPORT const unsigned char FreshlyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Freshly/PublicHeader.h>
+
+
